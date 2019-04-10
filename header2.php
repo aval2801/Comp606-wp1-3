1ee0069e1e5c0610b297541595b7365d8a55a042
AVAL <gohil.aval@gmail.com>
2019-04-10 21:57:08 +1200

    commit 2

--- header2.php
+++ header2.php
@@ -2,9 +2,7 @@
   session_start(); 
 ?>
 
-<!DOCTYPE html>
 <html>
-<hehttps://codeanywhere.com/editor/#ad>
   <meta charset="utf-8">
 	<title>Home</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
